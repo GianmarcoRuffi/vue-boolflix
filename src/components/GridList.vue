a8868e2ae3f098ede4601ca799d6d70d2f238585
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-11 22:10:58 +0200

    Prova flag

--- src/components/GridList.vue
+++ src/components/GridList.vue
@@ -16,6 +16,8 @@
         Titolo: {{ item.title ? item.title : item.name }}<br />
         Lingua: {{ item.original_language }}<br />
 
+        <img :src="getFlag(item.original_language)" />
+
         Voto: {{ item.vote_average }}<br />
       </li>
     </ul>
@@ -39,7 +41,26 @@ export default {
     //     return state.search
     // }
   },
-  methods: {},
+  methods: {
+    getFlag(flag) {
+      switch (flag) {
+        case "en":
+          return "https://flagcdn.com/16x12/uk.png";
+        case "it":
+          return "https://flagcdn.com/16x12/it.png";
+        case "fr":
+          return "https://flagcdn.com/16x12/fr.png";
+        case "es":
+          return "https://flagcdn.com/16x12/es.png";
+        case "de":
+          return "https://flagcdn.com/16x12/de.png";
+        case "ru":
+          return "https://flagcdn.com/16x12/ru.png";
+        default:
+          return flag;
+      }
+    },
+  },
 };
 </script>
 
