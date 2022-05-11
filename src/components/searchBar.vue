396ab57499b1109ed4a5a3e3cc0c3694f6507cb2
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-11 10:15:21 +0200

    init 4

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,12 +1,15 @@
 <template>
-  <div></div>
+  <div>searchbar {{ state.search }}</div>
 </template>
 
 <script>
+import state from "../store.js";
 export default {
   name: "searchBar",
   data() {
-    return {};
+    return {
+      state,
+    };
   },
   methods: {},
 };
---
+++ src/store.js
@@ -0,0 +1,5 @@
+const state = {
+  search: "",
+};
+
+export default state;
